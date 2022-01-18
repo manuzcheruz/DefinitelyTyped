87398b867226872de884d93efb3a8c48fc4a2874
Kipkemoi Emmanuel <kipkemoi@Kipkemois-MacBook-Air.local>
2022-01-18 10:06:12 +0300

    adding complex components

--- types/react-reveal/index.d.ts
+++ types/react-reveal/index.d.ts
@@ -3,7 +3,9 @@
 // Definitions by: manuzcheruz <https://github.com/manuzcheruz>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface SimpleProps {
+import * as React from 'react';
+
+interface SimpleParams {
     duration: number;
     timeout: number;
     delay: number;
@@ -11,11 +13,40 @@ interface SimpleProps {
     forever: boolean;
 }
 
-export function Flash(props: SimpleProps): void;
-export function HeadShake(props: SimpleProps): void;
-export function Jello(props: SimpleProps): void;
-export function Jump(props: SimpleProps): void;
-export function Pulse(props: SimpleProps): void;
-export function RubberBand(props: SimpleProps): void;
-export function Shake(props: SimpleProps): void;
-export function Spin(props: SimpleProps): void;
+interface InAndOutParams extends SimpleParams {
+    out: boolean;
+    left: boolean;
+    right: boolean;
+    top: boolean;
+    bottom: boolean;
+    mirror: boolean;
+    opposite: boolean;
+}
+
+interface WrapParams {
+    props: any;
+    inEffect: boolean;
+    outEffect: any;
+    children: any;
+}
+
+export default function Bounce(params: InAndOutParams, ...props: any[]): void;
+export default function Fade(params: InAndOutParams, context: boolean, ...props: any[]): void;
+export default function Flip(params: InAndOutParams, ...props: any[]): void;
+export default function LightSpeed(params: InAndOutParams, ...props: any[]): void;
+export default function Roll(params: InAndOutParams, ...props: any[]): void;
+export default function Rotate(params: InAndOutParams, ...props: any[]): void;
+export default function Slide(params: InAndOutParams, ...props: any[]): void;
+export default function Zoom(params: InAndOutParams, ...props: any[]): void;
+
+export function Flash(params: SimpleParams): void;
+export function HeadShake(params: SimpleParams): void;
+export function Jello(params: SimpleParams): void;
+export function Jump(params: SimpleParams): void;
+export function Pulse(params: SimpleParams): void;
+export function RubberBand(params: SimpleParams): void;
+export function Shake(params: SimpleParams): void;
+export function Spin(params: SimpleParams): void;
+export function Swing(params: SimpleParams): void;
+export function Tada(params: SimpleParams): void;
+export function Wobble(params: SimpleParams): void;
